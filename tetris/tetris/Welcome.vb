62b977a1e07bb733366745903d8cab07c7d5b17c
Joshua Glass <joshuag1000@outlook.com>
2019-07-06 17:43:52 +0100

    after day 1

---
+++ tetris/tetris/Welcome.vb
@@ -0,0 +1,11 @@
+﻿Public Class Welcome
+    Private Sub Welcome_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    End Sub
+
+    Private Sub MaterialRaisedButton1_Click(sender As Object, e As EventArgs) Handles MaterialRaisedButton1.Click
+        TetrisGame.Show()
+        Me.Close()
+    End Sub
+
+
+End Class
